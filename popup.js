af66b04ff075dea98f47643048c989fa5de275db
Márton Salomváry <salomvary@gmail.com>
2010-04-08 19:00:57 +0200

    css, minimal error handling

--- popup.js
+++ popup.js
@@ -1,5 +1,13 @@
 $(function() {
-	$.get('http://iwiw.hu/pages/activity/activities.jsp', function(data) {
-		$('<div/>').html(data).find('ul.activities').appendTo('body');
+	$.get('http://iwiw.hu/pages/activity/activities.jsp', function(data, textStatus) {
+		var activities;
+		if(textStatus == 'success') {
+			activities = $('<div/>').html(data).find('ul.activities');
+		}
+		if(activities.length > 0) {
+			activities.appendTo('#container');
+		} else {
+			$('body').html('<p>Valami nem frankó. Talán nem vagy bejelentkezve az <a href="http://iwiw.hu" target="_blank">iWiW</a>-re?</p>');
+		}
 	});
 });
